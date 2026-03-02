98bd535c146e112b97fa6ad8b01932e6380d7a8e
Daijon Roberts <dmqroberts@Daijons-MacBook-Air.local>
2026-03-02 21:35:10 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <string>
 
+void repl() {
+  std::cout << "$ ";
+  std::string input;
+  std::getline(std::cin, input);
+
+  std::cout << input << ": command not found";
+  std::cout << std::endl;
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
-  std::string input;
-  std::getline(std::cin, input);
-  
-  std::cout << input << ": command not found";
+  while(true) {
+  repl();
+  }
 
 }
