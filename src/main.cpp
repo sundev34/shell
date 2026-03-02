9175b232c83232754e4810265cfa616895547c11
Daijon Roberts <dmqroberts@Daijons-MacBook-Air.local>
2026-03-02 21:56:01 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <string>
 
+enum class REPLReturnCode {
+  Success = 0,
+  Failure = 1,
+  Exit = 2,
+};
+
+
 // read, evaluate, print, loop
-bool repl() {
+REPLReturnCode repl() {
   std::cout << "$ ";
 
   std::string input;
   if (!std::getline(std::cin, input))
-    return false;
+    return REPLReturnCode::Failure;
 
+  if (input == "exit")
+    return REPLReturnCode::Exit;
+  
   std::cout << input << ": command not found" << "\n";
-  return true;
+  return REPLReturnCode::Success;
 }
 
 int main() {
@@ -20,6 +30,11 @@ int main() {
   std::cerr << std::unitbuf;
 
   while (true) {
-    repl();
+    REPLReturnCode returnCode = repl();
+    if (returnCode == REPLReturnCode::Failure) {
+      return 1;
+    } else if (returnCode == REPLReturnCode::Exit) {
+      return 0;
+    }
   }
 }
